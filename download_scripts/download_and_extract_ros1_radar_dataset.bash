f12d54c65c141293b54f73f07356b958ab451fb4
Vladimir Kubelka <kubelvla@gmail.com>
2025-09-08 20:16:42 +0200

    Useful scripts for downloading and extracting.

---
+++ download_scripts/download_and_extract_ros1_radar_dataset.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Download all the parts
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.001 -O ros1_noetic.7z.001
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.002 -O ros1_noetic.7z.002
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.003 -O ros1_noetic.7z.003
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.004 -O ros1_noetic.7z.004
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.005 -O ros1_noetic.7z.005
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.006 -O ros1_noetic.7z.006
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.007 -O ros1_noetic.7z.007
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.008 -O ros1_noetic.7z.008
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.009 -O ros1_noetic.7z.009
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.010 -O ros1_noetic.7z.010
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.011 -O ros1_noetic.7z.011
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.012 -O ros1_noetic.7z.012
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.013 -O ros1_noetic.7z.013 	
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros1_noetic.7z.014 -O ros1_noetic.7z.014
+
+# Extract
+7z x ros1_noetic.7z.001	
---
+++ download_scripts/download_and_extract_ros2_radar_dataset.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Download all the parts
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.001 -O ros2_jazzy.7z.001
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.002 -O ros2_jazzy.7z.002
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.003 -O ros2_jazzy.7z.003
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.004 -O ros2_jazzy.7z.004
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.005 -O ros2_jazzy.7z.005
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.006 -O ros2_jazzy.7z.006
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.007 -O ros2_jazzy.7z.007
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.008 -O ros2_jazzy.7z.008
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.009 -O ros2_jazzy.7z.009
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.010 -O ros2_jazzy.7z.010
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.011 -O ros2_jazzy.7z.011
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.012 -O ros2_jazzy.7z.012
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.013 -O ros2_jazzy.7z.013 	
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.014 -O ros2_jazzy.7z.014
+wget http://subtdata.felk.cvut.cz/tmp_radar_forest_dataset_release/ros2_jazzy.7z.015 -O ros2_jazzy.7z.015
+
+# Extract
+7z x ros2_jazzy.7z.001	
+
